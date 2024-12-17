f7fa86c78e00c5e6402081c14306cad017dbabfb
BIT <BIT@BIT1>
2024-12-17 13:29:35 +0900

    init commit

---
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -0,0 +1,28 @@
+package helloweb;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter(); // 빈 개행도 보냄. 이제 body 내용 적기
+		out.print("<h1>Hello World</h1>");
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response); // doget과 dopost를 한번에 처리하도록 구성
+	}
+
+}
